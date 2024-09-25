4426f39f0f98b87045d8c3ef086d6c0f5c551ebb
B-gooroom <j.sean1316@gmail.com>
2024-09-26 01:00:14 +0900

    FEAT: List comp 추가 및 적용

---
+++ src/components/List/List.tsx
@@ -0,0 +1,51 @@
+import Label from "../Label/Label";
+
+interface ListProps {
+  label: string;
+  title: string;
+  description: string;
+  comments: number;
+  views: number;
+  imageUrl?: string;
+}
+
+export default function List({
+  label,
+  title,
+  description,
+  comments,
+  views,
+  imageUrl,
+}: ListProps) {
+  return (
+    <div className="flex-col flex gap-8">
+      <Label size="small" background="ad-gray-100">
+        {label}
+      </Label>
+      <p className="text-body2">{title}</p>
+      <div className="flex gap-16 w-full border-b pb-12">
+        <div className="flex-col flex">
+          <span className="text-caption1 flex-grow">{description}</span>
+          <dl className="text-caption1 flex gap-2">
+            <dt>답변</dt>
+            <dd>{comments}</dd>
+            <span>･</span>
+            <dt>조회수</dt>
+            <dd>{views}</dd>
+          </dl>
+        </div>
+        {imageUrl && (
+          <div className="border rounded-2xl overflow-hidden w-[64px] h-[64px] flex-shrink-0">
+            {/* <Image
+              src={imageUrl}
+              alt="썸네일 이미지"
+              width={64}
+              height={64}
+              className="object-cover"
+            /> */}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- src/components/QnA/QnA.tsx
+++ src/components/QnA/QnA.tsx
@@ -1,9 +1,11 @@
 import Header from "../Header/Header";
+import List from "../List/List";
 import { Spacer } from "../Spacer/Spacer";
 import Banner from "./Banner/Banner";
 import Popular from "./Popular/Popular";
 
 const POPULAR_QUESTIONS = 3;
+const QNA_LIST = 5;
 
 export default function QnA() {
   return (
@@ -12,13 +14,28 @@ export default function QnA() {
       <section className="px-16 pt-16 flex-col flex gap-16">
         <p className="text-subtitle1">🙋🏻 오늘의 질문</p>
         {Array.from({ length: POPULAR_QUESTIONS }).map((_, index) => (
-          <Popular key={index} />
+          <Popular key={index} index={index + 1} />
         ))}
       </section>
       <Spacer className="h-32" />
       <div className="px-16">
         <Banner />
       </div>
+      <Spacer className="h-32" />
+      <section className="px-16 pt-16 flex-col flex gap-16">
+        <p className="text-subtitle1">🔥 새로운 질문</p>
+        {Array.from({ length: QNA_LIST }).map((_, index) => (
+          <List
+            key={index}
+            title="사업자 등록증 신고할 때 궁금한 점"
+            label="자유"
+            description="이번에 배민이랑 인스타 광고 돌리는데 비용이 너무 많이 나와서 고민이네요 다른 분들은 어떻게 얼마나 나오시..."
+            comments={3}
+            views={900}
+            imageUrl="/images/1.jpg"
+          />
+        ))}
+      </section>
     </div>
   );
 }
