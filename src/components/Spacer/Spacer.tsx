dd1c10b210dfe24ae97b4c5d00d0c08f1d456032
B-gooroom <j.sean1316@gmail.com>
2024-09-26 17:54:36 +0900

    FIX: 빌드 에러 수정

--- src/components/Spacer/Spacer.tsx
+++ src/components/Spacer/Spacer.tsx
@@ -1,32 +1,32 @@
-import { forwardRef } from "react";
+// import { forwardRef } from "react";
 
-export const Spacer = forwardRef<
-  HTMLDivElement,
-  React.ComponentPropsWithoutRef<"div">
->(({ className, children, role = "presentation", ...rest }, ref) => {
-  return (
-    <div role={role} className={className} {...rest} ref={ref}>
-      {children}
-    </div>
-  );
-});
-
-// import React from "react";
-
-// export function Spacer(
-//   {
-//     className,
-//     children,
-//     role = "presentation",
-//     ...rest
-//   }: React.ComponentPropsWithoutRef<"div">,
-//   ref: React.Ref<HTMLDivElement>
-// ) {
+// export const Spacer = forwardRef<
+//   HTMLDivElement,
+//   React.ComponentPropsWithoutRef<"div">
+// >(({ className, children, role = "presentation", ...rest }, ref) => {
 //   return (
 //     <div role={role} className={className} {...rest} ref={ref}>
 //       {children}
 //     </div>
 //   );
-// }
+// });
 
-// export default React.forwardRef(Spacer);
+import React from "react";
+
+export function Spacer(
+  {
+    className,
+    children,
+    role = "presentation",
+    ...rest
+  }: React.ComponentPropsWithoutRef<"div">,
+  ref: React.Ref<HTMLDivElement>
+) {
+  return (
+    <div role={role} className={className} {...rest} ref={ref}>
+      {children}
+    </div>
+  );
+}
+
+export default React.forwardRef(Spacer);
