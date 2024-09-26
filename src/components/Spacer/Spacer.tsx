fe9b4c014067da69f3387f59364f87d9e3dd3d1c
B-gooroom <j.sean1316@gmail.com>
2024-09-26 18:01:50 +0900

    FIX: Spacer 형식 수정

--- src/components/Spacer/Spacer.tsx
+++ src/components/Spacer/Spacer.tsx
@@ -1,32 +1,15 @@
-// import { forwardRef } from "react";
-
-// export const Spacer = forwardRef<
-//   HTMLDivElement,
-//   React.ComponentPropsWithoutRef<"div">
-// >(({ className, children, role = "presentation", ...rest }, ref) => {
-//   return (
-//     <div role={role} className={className} {...rest} ref={ref}>
-//       {children}
-//     </div>
-//   );
-// });
-
 import React from "react";
 
-export function Spacer(
-  {
-    className,
-    children,
-    role = "presentation",
-    ...rest
-  }: React.ComponentPropsWithoutRef<"div">,
-  ref: React.Ref<HTMLDivElement>
-) {
+// 일반 함수형 컴포넌트
+export function Spacer({
+  className,
+  children,
+  role = "presentation",
+  ...rest
+}: React.ComponentPropsWithoutRef<"div">) {
   return (
-    <div role={role} className={className} {...rest} ref={ref}>
+    <div role={role} className={className} {...rest}>
       {children}
     </div>
   );
 }
-
-export default React.forwardRef(Spacer);
